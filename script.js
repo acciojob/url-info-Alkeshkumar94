dbe2ed0a61457bb4321e695a86ca2cfcc8b24e41
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-01-05 21:42:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 //your JS code here. If required.
 var cururl=window.location.href;
 var length=cururl.length;
-alert("The length of the URL is:" + length);
+alert("The length of the URL is: " + length);
